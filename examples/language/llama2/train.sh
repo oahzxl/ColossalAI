974c6037df085e55173787a50870ff82148525b0
Xuanlei Zhao <xuanlei.zhao@gmail.com>
2024-02-04 07:52:56 +0000

    Update dataset and training script***

--- examples/language/llama2/train.sh
+++ examples/language/llama2/train.sh
@@ -1,3 +1,5 @@
 device="0"
 
 CUDA_VISIBLE_DEVICES=$device torchrun --nproc_per_node 1 --standalone pretrain.py --plugin zero2 --batch_size 32 --config base --lr 3e-4 --max_length 1024 --flash_attention --warmup_steps 2000 --mixed_precision bf16 --save_interval 10000
+CUDA_VISIBLE_DEVICES=$device torchrun --nproc_per_node 1 --standalone pretrain.py --plugin zero2 --batch_size 32 --config base --lr 1e-4 --max_length 1024 --flash_attention --warmup_steps 2000 --mixed_precision bf16 --save_interval 10000
+CUDA_VISIBLE_DEVICES=$device torchrun --nproc_per_node 1 --standalone pretrain.py --plugin zero2 --batch_size 32 --config base --lr 1e-3 --max_length 1024 --flash_attention --warmup_steps 2000 --mixed_precision bf16 --save_interval 10000
